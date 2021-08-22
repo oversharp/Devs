d51c5d7bdd28d48c4534652ed38039022063de3f
José García <ja.garciac18@gmail.com>
2021-08-22 11:09:06 -0600

    Change docs

---
+++ Maps/Tiles/Tiles32/Room_Builder_Office_16x16.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Modern_Office_Black_Shadow_32x32" tilewidth="32" tileheight="32" tilecount="848" columns="16">
+ <image source="../../../TileSet/Modern_Office_Revamped_v1.0/Modern_Office_Revamped/2_Modern_Office_Black_Shadow/Modern_Office_Black_Shadow_32x32.png" width="512" height="1696"/>
+</tileset>
