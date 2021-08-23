2f767274048a8a36e263c53f300234e1ca2a1f47
Mario L <42539700+maluxz@users.noreply.github.com>
2021-08-22 19:39:54 -0600

    Cambio
    Hice algo

---
+++ ProyectoMinerDev/Assets/SuperTiled2Unity/Import/Tiles/Modern_Office_16x16.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Modern_Office_16x16" tilewidth="16" tileheight="16" tilecount="848" columns="16">
+ <image source="../../TileSet/Modern_Office_Revamped_v1.0/Modern_Office_Revamped/Modern_Office_16x16.png" width="256" height="848"/>
+</tileset>
