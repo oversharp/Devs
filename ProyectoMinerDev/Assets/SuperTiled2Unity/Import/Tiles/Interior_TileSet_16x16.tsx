302cfcdd455b2081efa999f7ed91005b257548d5
José García <ja.garciac18@gmail.com>
2021-08-22 20:51:14 -0600

    TestForTiles

--- ProyectoMinerDev/Assets/SuperTiled2Unity/Import/Tiles/Interior_TileSet_16x16.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="Interior_TileSet_16x16" tilewidth="16" tileheight="16" tilecount="2304" columns="48">
- <image source="../../TileSet/daeqz28-55f6b790-f57e-4dc0-9cac-96666290fc23.png" width="768" height="768"/>
-</tileset>
