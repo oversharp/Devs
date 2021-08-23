af8896eac15379848351092eff49441e886658ac
José García <ja.garciac18@gmail.com>
2021-08-22 20:05:01 -0600

    Inicial map

--- Maps/Tiles/Room_Builder_Office_16x16.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="Room_Builder_Office_16x16" tilewidth="16" tileheight="16" tilecount="224" columns="16">
- <image source="../../TileSet/Modern_Office_Revamped_v1.0/Modern_Office_Revamped/1_Room_Builder_Office/Room_Builder_Office_16x16.png" width="256" height="224"/>
-</tileset>
---
+++ ProyectoMinerDev/Assets/SuperTiled2Unity/Import/Room_Builder_baseboards_32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Room_Builder_baseboards_32x32" tilewidth="32" tileheight="32" tilecount="36" columns="6">
+ <image source="../../../../TileSet/Modern_Interiors/1_Interiors/32x32/Room_Bulder_subfiles_32x32/Room_Builder_baseboards_32x32.png" width="192" height="192"/>
+</tileset>
